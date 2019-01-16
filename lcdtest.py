ae5a05e9d635dd180f53873002bcac11fce67977
Paul Wichser <paul+github@paulwichser.com>
2019-01-16 14:38:06 -0600

    lcd test code, 1st layout meeting release

--- lcdtest.py
+++ lcdtest.py
@@ -7,4 +7,4 @@ i2c = busio.I2C(board.SCL, board.SDA)
 lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
 lcd.clear
 lcd.color = [100, 0, 0]
-lcd.message = "Hello!"
+lcd.message = "  Loading..."
